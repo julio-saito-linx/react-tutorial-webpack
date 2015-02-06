ba548a9001051495ca6a4106ebe4c03d083b6502
Julio Makdisse Saito <saitodisse@gmail.com>
2015-02-06 19:29:01 -0200

    Using props

--- src/scripts/components/Comment.js
+++ src/scripts/components/Comment.js
@@ -7,12 +7,15 @@ require('../../styles/Comment.css');
 var Comment = React.createClass({
   render: function () {
     return (
-        <div>
-          <p>Content for Comment</p>
-        </div>
+        <div className="comment">
+	        <h2 className="commentAuthor">
+	          	{this.props.author}
+	        </h2>
+	        {this.props.children}
+	    </div>
       );
   }
 });
 
-module.exports = Comment; 
+module.exports = Comment;
 
