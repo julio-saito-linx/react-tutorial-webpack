393a62510cacaef7a832be289b3d5d0137d1305d
Julio Makdisse Saito <saitodisse@gmail.com>
2015-02-07 15:40:30 -0200

    Styling

--- src/scripts/components/Comment.js
+++ src/scripts/components/Comment.js
@@ -10,10 +10,18 @@ var Comment = React.createClass({
   	var rawMarkup = marked(this.props.children.toString());
     return (
         <div className="Comment">
-	        <h2 className="commentAuthor">
-	          	{ this.props.author }
-	        </h2>
-	        <span className="commentBody" dangerouslySetInnerHTML={{__html: rawMarkup}} />
+          <div className="section group">
+            <div className="col span_3_of_4">
+    	        <h2 className="commentAuthor">
+    	          	{ this.props.author }
+    	        </h2>
+    	        <span className="commentBody" dangerouslySetInnerHTML={{__html: rawMarkup}} />
+            </div>
+            <div className="col span_1_of_4">
+              <button type="button" id="remove_button">remove this comment</button>
+            </div>
+          </div>
+
 	    </div>
       );
   }
